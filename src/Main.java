a2512fac7230e4bf45690b7ed076e4b098f58ba1
toth-j <tothj123@hotmail.com>
2023-10-15 21:53:50 +0200

    ij

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Kiírások
+        System.out.println("Helló világ!");
+        System.out.println("Második sor.");     // megjegyzés
+//        System.out.println("Harmadik sor...");
+    }
+}
